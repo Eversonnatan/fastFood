c305cd8a4201d4cfa0a1b4375b3b6f8fe4aaf543
eversonnatan <everson.natan@gmail.com>
2022-11-17 19:55:55 -0300

    fragmentos2

---
+++ app/src/main/java/br/com/etecia/fastfood/LanchesFragment.java
@@ -0,0 +1,30 @@
+package br.com.etecia.fastfood;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LanchesFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       View view = inflater.inflate(R.layout.fragment_lanches, container, false);
+
+
+
+
+
+
+
+        return  view;
+    }
+}
