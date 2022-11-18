7fe0fd7fa5ad83a4943a363e92580ef839c4a2c4
eversonnatan <everson.natan@gmail.com>
2022-11-17 22:13:38 -0300

    Criando list view w fragmentos

---
+++ app/src/main/java/br/com/etecia/fastfood/PIzzaActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.fastfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PIzzaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pizza_layout);
+    }
+}
